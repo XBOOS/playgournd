fc81e2f9024950a988c4b64d4a816c6b8e8cb193
xubing <xubinghku@gmail.com>
2015-11-23 13:53:10 +0800

    haskell list comprehensions

---
+++ utils/haskell/listComprehensions.hs
@@ -0,0 +1,18 @@
+module ListComprehension where
+import Prelude
+-- some utils to use list comprehensions
+factors :: Int ->[Int]
+factors n = 
+  [x|x<-[1..n],n `mod` x==0]
+
+prime :: Int -> Bool --to determine wether an int is prime nubmer
+--the facotrs are only 1 and itself
+prime n = factors n == [1,n]
+
+primes :: Int -> [Int]
+primes n = [x | x<- [2..n] , prime x]
+
+--Testing
+--facotrs 10
+--prime 7
+--primes 56
