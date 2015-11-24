4c8e52ea6137aea496a57b38ed30622c920339db
xubing <xubinghku@gmail.com>
2015-11-24 22:34:43 +0800

    parser part1

---
+++ utils/haskell/parser.hs
@@ -0,0 +1,24 @@
+module Parser where 
+import Prelude hiding (return)
+
+type Parser a = String-> [(a,String)]
+
+item :: Parser Char
+item = \input -> case input of 
+  [] -> []
+  (x:xs) -> [(x,xs)]
+
+failure :: Parser a
+failure = \input -> []
+
+return :: a->Parser a
+return v = \input -> [(v,input)]
+
+(+++) :: Parser a ->Parser a->Parser a
+p +++ q = \input -> case  parse p input of 
+  [] -> parse q input
+  [(v,out)] -> [(v,out)]
+
+
+parse :: Parser a -> String -> [(a,String)]
+parse p input = p input
